4154fa7ba47f0a20f3a8f14c887e45486bdd34f0
WRHR <wrhreeves@gmail.com>
2021-03-16 20:46:01 -0600

    convert to ts

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  // homepage
+  // cookbook
+  // add recipe
+  //modify existing recipe
+  // search your recipes
+  return <div className="App"></div>;
+}
+
+export default App;
