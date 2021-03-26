0f9322d207697368fd6631d55310afc29395c5a6
WRHR <wrhreeves@gmail.com>
2021-03-25 23:55:16 -0600

    Import and start login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import React from "react";
+import { Box } from "@material-ui/core";
+import { Form, Formik } from "formik";
+import { useLoginMutation } from "../generated/graphql";
 
+interface Values {
+  usernameOrEmail: string;
+  password: string;
+}
 
 export const login: React.FC = ({}) => {
-    return ();
-}
+  return (
+    <Box>
+      <Formik
+        initialValues={{
+          usernameOrEmail: "",
+          password: "",
+        }}
+        onSubmit={(values:Values, useLoginMutation())}
+      >
+        <Form></Form>
+      </Formik>
+    </Box>
+  );
+};
