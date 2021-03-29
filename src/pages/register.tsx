ecd83927e0458f34dbbc4daec8a7fd7df28afec1
WRHR <wrhreeves@gmail.com>
2021-03-28 21:47:00 -0600

    Update register page

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,5 +1,64 @@
-import React from 'react'
-
+import React from "react";
+import { useRouter } from "next/router";
+import { MeDocument, MeQuery, useRegisterMutation } from "../generated/graphql";
+import { Box, Button, CircularProgress } from "@material-ui/core";
+import { Form, Formik } from "formik";
+import { toErrorMap } from "../utils/toErrorMap";
+import { InputField } from "../components/InputField";
 export const register: React.FC = ({}) => {
-    return ();
-}
+  const router = useRouter();
+  const [register] = useRegisterMutation();
+
+  return (
+    <Box>
+      <Formik
+        initialValues={{ email: "", username: "", password: "" }}
+        onSubmit={async (values, { setErrors }) => {
+          const response = await register({
+            variables: { options: values },
+            update: (cache, { data }) => {
+              cache.writeQuery<MeQuery>({
+                query: MeDocument,
+                data: {
+                  __typename: "Query",
+                  me: data?.register.user,
+                },
+              });
+            },
+          });
+          if (response.data?.register.errors) {
+            setErrors(toErrorMap(response.data.register.errors));
+          } else if (response.data?.register.user) {
+            router.push("/");
+          }
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="username"
+              placeholder="Username"
+              lable="Username"
+            />
+            <Box mt={4}>
+              <InputField name="email" placeholder="Email" lable="Email" />
+            </Box>
+            <Box mt={4}>
+              <InputField
+                name="password"
+                placeholder="Password"
+                lable="Password"
+                type="password"
+              />
+            </Box>
+            {isSubmitting ? (
+              <Button type="submit">register</Button>
+            ) : (
+              <CircularProgress />
+            )}
+          </Form>
+        )}
+      </Formik>
+    </Box>
+  );
+};
