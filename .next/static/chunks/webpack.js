53df30695164cec2097c6701b5d7dcf612f153c9
WRHR <wrhreeves@gmail.com>
2021-04-02 23:27:53 -0600

    Add style and link to register page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f5b1805fe55d282d6e3b";
+/******/ 	var hotCurrentHash = "b62b45ab70f55b4b796a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,17 +1,41 @@
 import React from "react";
 import { useRouter } from "next/router";
 import { MeDocument, MeQuery, useRegisterMutation } from "../generated/graphql";
-import { Box, Button, CircularProgress } from "@material-ui/core";
+import {
+  Box,
+  Button,
+  CircularProgress,
+  createStyles,
+  makeStyles,
+  Theme,
+  Typography,
+} from "@material-ui/core";
 import { Form, Formik } from "formik";
 import { toErrorMap } from "../utils/toErrorMap";
 import { InputField } from "../components/InputField";
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      marginTop: "20%",
+    },
+    page: {
+      height: "100%",
+      width: "100%",
+    },
+  })
+);
+
 const register: React.FC = ({}) => {
   const router = useRouter();
   const [register] = useRegisterMutation();
+  const classes = useStyles();
 
   return (
-    <Box>
+    <Box className={classes.page}>
       <Formik
         initialValues={{ email: "", username: "", password: "" }}
         onSubmit={async (values, { setErrors }) => {
@@ -35,29 +59,34 @@ const register: React.FC = ({}) => {
         }}
       >
         {({ isSubmitting }) => (
-          <Form>
-            <InputField
-              name="username"
-              placeholder="Username"
-              lable="Username"
-            />
-            <Box mt={4}>
-              <InputField name="email" placeholder="Email" lable="Email" />
-            </Box>
-            <Box mt={4}>
+          <Box className={classes.container}>
+            <Form>
               <InputField
-                name="password"
-                placeholder="Password"
-                lable="Password"
-                type="password"
+                name="username"
+                placeholder="Username"
+                lable="Username"
               />
-            </Box>
-            {isSubmitting ? (
-              <CircularProgress />
-            ) : (
-              <Button type="submit">register</Button>
-            )}
-          </Form>
+              <Box mt={4}>
+                <InputField name="email" placeholder="Email" lable="Email" />
+              </Box>
+              <Box mt={4}>
+                <InputField
+                  name="password"
+                  placeholder="Password"
+                  lable="Password"
+                  type="password"
+                />
+              </Box>
+              {isSubmitting ? (
+                <CircularProgress />
+              ) : (
+                <Button type="submit">register</Button>
+              )}
+            </Form>
+            <Typography>
+              Already have an account? <a href="/login">Click Here</a>
+            </Typography>
+          </Box>
         )}
       </Formik>
     </Box>
