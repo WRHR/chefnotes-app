d2fbd93ac0dbbdd990347699827a6253b24681f5
WRHR <wrhreeves@gmail.com>
2021-04-09 22:54:43 -0600

    Edit card style

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -1,20 +1,15 @@
-import React from 'react'
-import { Button, Text } from '@chakra-ui/react'
+import React from "react";
+import { Box, Button, Flex, Text } from "@chakra-ui/react";
 
-interface RecipeCardProps {
-
-}
+interface RecipeCardProps {}
 
 export const RecipeCard: React.FC<RecipeCardProps> = ({}) => {
-    return (
-      <Card>
-        <CardContent>
-          <Text>Recipe Name</Text>
-          <Text>Recipe decription</Text>
-        </CardContent>
-    
-          <Button>View Recipe</Button>
-       
-      </Card>
-    );
-}
+  return (
+    <Flex flexDir='column'>
+      <Text>Recipe Name</Text>
+      <Text>Recipe decription</Text>
+
+      <Button>View Recipe</Button>
+    </Flex>
+  );
+};
