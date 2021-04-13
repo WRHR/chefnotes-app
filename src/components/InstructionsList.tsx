151d07d152a10efda7bdfc6916e02ce94bb80a9a
WRHR <wrhreeves@gmail.com>
2021-04-12 22:59:30 -0600

    Create instruction list component

---
+++ src/components/InstructionsList.tsx
@@ -0,0 +1,23 @@
+import { Box , Text} from '@chakra-ui/layout';
+import React from 'react'
+import { useRecipeInstructionsQuery } from '../generated/graphql';
+
+interface InstructionsListProps {
+  id:number
+  original:boolean
+}
+
+export const InstructionsList: React.FC<InstructionsListProps> = ({id, original}) => {
+  const {data, loading, error} = useRecipeInstructionsQuery({id, original})
+
+  let instructionMap = data?.recipeInstructions.map((instruction)=>{
+    return (
+      <Text>{instruction.position} {instruction.description}</Text>
+    )
+  })
+    return (
+      <Box>
+        {instructionMap}
+      </Box>
+    );
+}
