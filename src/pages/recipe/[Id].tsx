c80a6317a56ddc4bbcc50d5d82d48399753c7028
WRHR <wrhreeves@gmail.com>
2021-04-12 23:11:06 -0600

    Remove unused imports and spacing

--- src/pages/recipe/[Id].tsx
+++ src/pages/recipe/[Id].tsx
@@ -1,34 +1,28 @@
-import { Box, Text } from '@chakra-ui/layout';
-import React from 'react'
-import { InstructionsList } from '../../components/InstructionsList';
-import { useRecipeIngredientsQuery, useRecipeInstructionsQuery } from '../../generated/graphql';
-import { useGetRecipeFromUrl } from '../../utils/useGetRecipeFromUrl';
-
-
+import { Box, Text } from "@chakra-ui/layout";
+import React from "react";
+import { InstructionsList } from "../../components/InstructionsList";
+import { useGetRecipeFromUrl } from "../../utils/useGetRecipeFromUrl";
 
 export const Recipe = ({}) => {
-  const {data, error, loading} = useGetRecipeFromUrl()
-  
-  if(loading){
-    return (<div>loading...</div>)
+  const { data, error, loading } = useGetRecipeFromUrl();
+
+  if (loading) {
+    return <div>loading...</div>;
   }
 
-  if(error){
-    return <div>{error.message}</div>
+  if (error) {
+    return <div>{error.message}</div>;
   }
 
-  if(!data?.baseRecipe){
-    return(
-      <div>Could not find recipe</div>
-    )
+  if (!data?.baseRecipe) {
+    return <div>Could not find recipe</div>;
   }
 
-  
-    return (
-      // base recipe data
-      <Box>
-        <Text>{data.baseRecipe.name}</Text>
-        <InstructionsList id={data.baseRecipe.id} original={true} />
-      </Box>
-    );
-}
+  return (
+    // base recipe data
+    <Box>
+      <Text>{data.baseRecipe.name}</Text>
+      <InstructionsList id={data.baseRecipe.id} original={true} />
+    </Box>
+  );
+};
