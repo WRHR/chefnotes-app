2d3103e10b7b535f79dfc631ea23cb6e22be1efe
WRHR <wrhreeves@gmail.com>
2021-04-12 22:47:57 -0600

    Add instructions query

--- src/pages/recipe/[Id].tsx
+++ src/pages/recipe/[Id].tsx
@@ -1,5 +1,6 @@
 import { Box } from '@chakra-ui/layout';
 import React from 'react'
+import { useRecipeIngredientsQuery, useRecipeInstructionsQuery } from '../../generated/graphql';
 import { useGetRecipeFromUrl } from '../../utils/useGetRecipeFromUrl';
 
 
@@ -19,6 +20,14 @@ export const Recipe = ({}) => {
     return(
       <div>Could not find recipe</div>
     )
+  }
+
+  if(data.baseRecipe){
+    let baseOptions = {
+      id:data.baseRecipe.id,
+      original:true
+    }
+    const {data, loading, error} = useRecipeIngredientsQuery()
   }
     return (
       // base recipe data
