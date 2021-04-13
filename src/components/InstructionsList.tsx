d1e05b1da81fda2c1698a81a3a9dff2428f3c4da
WRHR <wrhreeves@gmail.com>
2021-04-12 23:08:19 -0600

    Add type to base options

--- src/components/InstructionsList.tsx
+++ src/components/InstructionsList.tsx
@@ -1,23 +1,34 @@
-import { Box , Text} from '@chakra-ui/layout';
-import React from 'react'
-import { useRecipeInstructionsQuery } from '../generated/graphql';
+import { QueryHookOptions } from "@apollo/client";
+import { Box, Text } from "@chakra-ui/layout";
+import React from "react";
+import {
+  Exact,
+  RecipeInstructionsQuery,
+  useRecipeInstructionsQuery,
+} from "../generated/graphql";
 
 interface InstructionsListProps {
-  id:number
-  original:boolean
+  id: number;
+  original: boolean;
 }
 
-export const InstructionsList: React.FC<InstructionsListProps> = ({id, original}) => {
-  const {data, loading, error} = useRecipeInstructionsQuery({id, original})
+export const InstructionsList: React.FC<InstructionsListProps> = ({
+  id,
+  original,
+}) => {
+  const baseOptions: QueryHookOptions<
+    RecipeInstructionsQuery,
+    Exact<{ id: number; original: boolean }>
+  > = { id, original };
 
-  let instructionMap = data?.recipeInstructions.map((instruction)=>{
+  const { data, loading, error } = useRecipeInstructionsQuery(baseOptions);
+
+  let instructionMap = data?.recipeInstructions.map((instruction) => {
     return (
-      <Text>{instruction.position} {instruction.description}</Text>
-    )
-  })
-    return (
-      <Box>
-        {instructionMap}
-      </Box>
+      <Text>
+        {instruction.position} {instruction.description}
+      </Text>
     );
-}
+  });
+  return <Box>{instructionMap}</Box>;
+};
