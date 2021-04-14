99517386bcb48e5c3cc42e77bb83e71c4ef79752
WRHR <wrhreeves@gmail.com>
2021-04-13 23:13:58 -0600

    Add new recipe page

---
+++ src/pages/recipe/NewRecipe.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Formik, Form } from "formik";
+import { Box, Button } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { useCreateBaseRecipeMutation } from "../../generated/graphql";
+import { InputField } from "../../components/InputField";
+
+interface NewRecipeProps {}
+
+export const NewRecipe: React.FC<NewRecipeProps> = ({}) => {
+  const router = useRouter();
+  return (
+    <Box>
+      <Formik
+        initialValues={{ name: "", description: "" }}
+        onSubmit={async (values) => {
+          const { errors } = await useCreateBaseRecipeMutation({
+            variables: { input: values },
+          });
+          if (!errors) {
+            router.push("/");
+          }
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="name"
+              placeholder="Recipe Name"
+              label="Recipe Name"
+            />
+            <Box>
+              <InputField
+                name="description"
+                placeholder="Recipe Description"
+                label="Recipe Description"
+              />
+            </Box>
+            <Button type="submit" isLoading={isSubmitting}>
+              Create Recipe
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Box>
+  );
+};
