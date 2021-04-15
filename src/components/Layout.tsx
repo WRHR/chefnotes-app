9b79ef4d8bda01ff8a473712866fd3b04c111d03
WRHR <wrhreeves@gmail.com>
2021-04-14 21:57:42 -0600

    Add layout and wrapper to simplify nav and layout

---
+++ src/components/Layout.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { NavBar } from './NavBar'
+import { Wrapper } from './Wrapper'
+
+interface LayoutProps {
+
+}
+
+export const Layout: React.FC<LayoutProps> = ({children}) => {
+    return (
+      <>
+        <NavBar />
+        <Wrapper>
+          {children}
+        </Wrapper>
+      </>
+    );
+}
