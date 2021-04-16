a687b8ce480e39faa48dbffed8a15c4846afb788
WRHR <wrhreeves@gmail.com>
2021-04-15 22:22:57 -0600

    Remove unused inmports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,4 @@
-import { useRouter } from "next/router";
 import { Layout } from "../components/Layout";
-import { NavBar } from "../components/NavBar";
-import { useMeQuery } from "../generated/graphql";
 
 export default function Homepage() {
   return <Layout></Layout>;
