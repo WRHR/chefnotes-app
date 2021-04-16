928917f95321c4cb4bc242e6d215695af4efe856
WRHR <wrhreeves@gmail.com>
2021-04-15 22:22:13 -0600

    Add layout to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,8 @@
 import { useRouter } from "next/router";
+import { Layout } from "../components/Layout";
 import { NavBar } from "../components/NavBar";
 import { useMeQuery } from "../generated/graphql";
 
-export default function Homepage(){
-
-
-  return (
-    <div>
-      <NavBar />
-    </div>
-  )
+export default function Homepage() {
+  return <Layout></Layout>;
 }
-
