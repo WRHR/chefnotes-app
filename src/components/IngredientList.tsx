c94a121a9873037815e31c57c8f8efd1da429bb1
WRHR <wrhreeves@gmail.com>
2021-04-21 22:39:53 -0600

    Set up Editing options for ingredient list

--- src/components/IngredientList.tsx
+++ src/components/IngredientList.tsx
@@ -1,5 +1,5 @@
 import { Box, List, ListItem, Text } from '@chakra-ui/layout';
-import React from 'react'
+import React, { useState } from 'react'
 import { useRecipeIngredientsQuery } from '../generated/graphql';
 
 interface IngredientListProps {
@@ -9,12 +9,14 @@ interface IngredientListProps {
 
 export const IngredientList: React.FC<IngredientListProps> = ({id, original}) => {
   const {data, loading, error} = useRecipeIngredientsQuery({variables:{id, original}})
+  const [selectedIngredient, useSelectedIngredient] = useState(0)
 
   const ingregientMap = data?.recipeIngredients.map((ingredient)=>{
     return(
-      <ListItem>
+      <ListItem onClick={()=>useSelectedIngredient(ingredient.id)}>
         {ingredient.quantity} {ingredient.name}
         {/* on click modify options */}
+        {selectedIngredient===ingredient.id?<Box>Edit options</Box>:null}
         </ListItem>
     )
   })
