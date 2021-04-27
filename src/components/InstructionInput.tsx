37c1a38718b1ecd5607b3395b3f4f619369304b1
WRHR <wrhreeves@gmail.com>
2021-04-26 22:14:09 -0600

    delete edit options, make instructioninput

---
+++ src/components/InstructionInput.tsx
@@ -0,0 +1,14 @@
+import { Formik } from 'formik';
+import React from 'react'
+
+interface InstructionInputProps {
+
+}
+
+export const InstructionInput: React.FC<InstructionInputProps> = ({}) => {
+    return (
+      <Formik>
+        
+      </Formik>
+    );
+}
