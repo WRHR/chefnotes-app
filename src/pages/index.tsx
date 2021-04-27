ca947c28aa28223be6ca87577876d7e62ba4b4b1
WRHR <wrhreeves@gmail.com>
2021-04-26 22:18:31 -0600

    Start recipe index page layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
 import { Layout } from "../components/Layout";
 
 export default function Homepage() {
-  return <Layout></Layout>;
+  return <Layout>
+    {/* Search bar */}
+    {/* User Recipe list */}
+    {/* New recipe */}
+  </Layout>;
 }
