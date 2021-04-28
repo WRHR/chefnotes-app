1d4342b951fd4ec08ec767629fd330537170a5f5
WRHR <wrhreeves@gmail.com>
2021-04-27 22:52:24 -0600

    Edit instruction input form

--- src/components/InstructionInput.tsx
+++ src/components/InstructionInput.tsx
@@ -1,14 +1,34 @@
-import { Formik } from 'formik';
-import React from 'react'
+import { Formik } from "formik";
+import React from "react";
+import { useCreateInstructionMutation } from "../generated/graphql";
+import { InputField } from "./InputField";
 
 interface InstructionInputProps {
-
+  recipeId: number;
+  position: number;
+  original: boolean;
 }
 
-export const InstructionInput: React.FC<InstructionInputProps> = ({}) => {
-    return (
-      <Formik>
-        
-      </Formik>
-    );
-}
+export const InstructionInput: React.FC<InstructionInputProps> = ({
+  recipeId,
+  position,
+  original,
+}) => {
+  const [createInstruction] = useCreateInstructionMutation();
+  return (
+    <Formik
+      initialValues={{ description: "", position: position }}
+      onSubmit={async (values) => {
+        const { errors } = await createInstruction({
+          variables: { input: values, original, recipeId },
+        });
+      }}
+    >
+      <InputField
+        name="description"
+        label="description"
+        placeholder="Add an instruction for this recipe"
+      />
+    </Formik>
+  );
+};
