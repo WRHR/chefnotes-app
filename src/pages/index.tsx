72c7776eca029742338024e351d7ead1a3ccfe01
WRHR <wrhreeves@gmail.com>
2021-04-27 22:40:59 -0600

    Add new recipe link to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@chakra-ui/button";
+import { Link } from "@chakra-ui/layout";
 import { Layout } from "../components/Layout";
 
 export default function Homepage() {
@@ -5,5 +7,6 @@ export default function Homepage() {
     {/* Search bar */}
     {/* User Recipe list */}
     {/* New recipe */}
+    <Link href='/recipe/NewRecipe'>Add a Recipe</Link>
   </Layout>;
 }
