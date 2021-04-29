a2bc9283f98e990499e7c448c657b4b051b2e463
WRHR <wrhreeves@gmail.com>
2021-04-28 23:09:08 -0600

    Add update to instruction input

--- src/components/InstructionInput.tsx
+++ src/components/InstructionInput.tsx
@@ -1,27 +1,42 @@
 import { Formik } from "formik";
 import React from "react";
-import { useCreateInstructionMutation } from "../generated/graphql";
+import {
+  useCreateInstructionMutation,
+  useUpdateInstructionMutation,
+} from "../generated/graphql";
 import { InputField } from "./InputField";
 
 interface InstructionInputProps {
   recipeId: number;
   position: number;
   original: boolean;
+  edit: boolean;
+  instructionId?: number;
 }
 
 export const InstructionInput: React.FC<InstructionInputProps> = ({
   recipeId,
   position,
   original,
+  edit,
+  instructionId,
 }) => {
+  const [updateInstruction] = useUpdateInstructionMutation();
   const [createInstruction] = useCreateInstructionMutation();
+
   return (
     <Formik
       initialValues={{ description: "", position: position }}
       onSubmit={async (values) => {
-        const { errors } = await createInstruction({
-          variables: { input: values, original, recipeId },
-        });
+        if (edit && instructionId) {
+          const { errors } = await updateInstruction({
+            variables: { id: instructionId, input: values },
+          });
+        } else {
+          const { errors } = await createInstruction({
+            variables: { input: values, original, recipeId },
+          });
+        }
       }}
     >
       <InputField
