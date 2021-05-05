a893a81a6594dab372ee8aeb37dca5c97c7376ab
WRHR <wrhreeves@gmail.com>
2021-05-04 23:30:38 -0600

    Add userrecipe query to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,21 @@
 import { Button } from "@chakra-ui/button";
-import { Link } from "@chakra-ui/layout";
+import { Link, List, ListItem } from "@chakra-ui/layout";
 import { Layout } from "../components/Layout";
+import { useUserBaseRecipesQuery } from "../generated/graphql";
 
 export default function Homepage() {
+  const {data, loading, error} = useUserBaseRecipesQuery()
+  const userRecipesMap = data?.userBaseRecipes.map(recipe=>{
+    return(
+      <ListItem><Link href={`/recipe/${recipe.id}`}>{recipe.name}</Link></ListItem>
+    )
+  })
+
   return <Layout>
     {/* Search bar */}
-    {/* User Recipe list */}
+    <List>
+      {userRecipesMap}
+    </List>
 
     <Link href='/recipe/NewRecipe'>Add a Recipe</Link>
   </Layout>;
