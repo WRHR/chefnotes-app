7866d11f6d984f03e8904487fa3521a40fac8199
WRHR <wrhreeves@gmail.com>
2021-05-25 21:44:01 -0600

    Add maxwidth to register box

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -13,58 +13,60 @@ const register: React.FC = ({}) => {
 
   return (
     <Center>
-      <Heading />
-      <Formik
-        initialValues={{ email: "", username: "", password: "" }}
-        onSubmit={async (values, { setErrors }) => {
-          const response = await register({
-            variables: { options: values },
-            update: (cache, { data }) => {
-              cache.writeQuery<MeQuery>({
-                query: MeDocument,
-                data: {
-                  __typename: "Query",
-                  me: data?.register.user,
-                },
-              });
-            },
-          });
-          if (response.data?.register.errors) {
-            setErrors(toErrorMap(response.data.register.errors));
-          } else if (response.data?.register.user) {
-            router.push("/");
-          }
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Flex flexDir="column">
-            <Form>
-              <InputField
-                name="username"
-                placeholder="Username"
-                label="Username"
-              />
-              <Box mt={4}>
-                <InputField name="email" placeholder="Email" label="Email" />
-              </Box>
-              <Box mt={4}>
+      <Box maxW="500px">
+        <Heading />
+        <Formik
+          initialValues={{ email: "", username: "", password: "" }}
+          onSubmit={async (values, { setErrors }) => {
+            const response = await register({
+              variables: { options: values },
+              update: (cache, { data }) => {
+                cache.writeQuery<MeQuery>({
+                  query: MeDocument,
+                  data: {
+                    __typename: "Query",
+                    me: data?.register.user,
+                  },
+                });
+              },
+            });
+            if (response.data?.register.errors) {
+              setErrors(toErrorMap(response.data.register.errors));
+            } else if (response.data?.register.user) {
+              router.push("/");
+            }
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Flex flexDir="column">
+              <Form>
                 <InputField
-                  name="password"
-                  placeholder="Password"
-                  label="Password"
-                  type="password"
+                  name="username"
+                  placeholder="Username"
+                  label="Username"
                 />
-              </Box>
-              <Button type="submit" isLoading={isSubmitting}>
-                register
-              </Button>
-            </Form>
-            <Text>
-              Already have an account? <a href="/login">Click Here</a>
-            </Text>
-          </Flex>
-        )}
-      </Formik>
+                <Box mt={4}>
+                  <InputField name="email" placeholder="Email" label="Email" />
+                </Box>
+                <Box mt={4}>
+                  <InputField
+                    name="password"
+                    placeholder="Password"
+                    label="Password"
+                    type="password"
+                  />
+                </Box>
+                <Button type="submit" isLoading={isSubmitting}>
+                  register
+                </Button>
+              </Form>
+              <Text>
+                Already have an account? <a href="/login">Click Here</a>
+              </Text>
+            </Flex>
+          )}
+        </Formik>
+      </Box>
     </Center>
   );
 };
