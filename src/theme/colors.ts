beb785579a59aa5209d54cd81036bb93ba5564f1
WRHR <wrhreeves@gmail.com>
2021-06-10 22:31:09 -0600

    Change color themes, and use hsl values

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -1,6 +1,7 @@
 export const color = {
-  primary: "blue",
-  secondary: "red",
-  darkPrimary: "dark blue",
-  darkSecondary: "dark red",
+  primary: "hsl(200, 44, 39)",
+  secondary: "hsl(349, 53, 50)",
+  darkPrimary: "hsl(207, 50, 29)",
+  lightPrimary: "hsl(187, 75, 75)",
+  accent: "hsl(170, 74, 82)",
 };
